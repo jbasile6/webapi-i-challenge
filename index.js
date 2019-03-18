1b57b8925ba39679a25abd04eea17c6347c894a5
James Basile <jbasile145@gmail.com>
2019-03-18 15:51:22 -0400

    /api/users/:id GET request complete

--- index.js
+++ index.js
@@ -21,6 +21,21 @@ server.get('/api/users', (req, res) => {
         });
 });
 
+server.get('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    db.findById(id)
+        .then(user => {
+            if (!user) {
+                res.status(404).json({ message: "The user with the specified ID does not exist." })
+            } else {
+                res.status(200).json(user);
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ error: "The user information could not be retrieved." })
+        })
+})
+
 //At the bottom
 server.listen(4000, () => {
     console.log('\n** API up and running on port 4k **');
