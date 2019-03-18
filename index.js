346ee00dc2c22f055d2b7b908f1d2bb802fdd8d8
James Basile <jbasile145@gmail.com>
2019-03-18 15:45:09 -0400

    C:/Program Files/Git/api/users GET complete

--- index.js
+++ index.js
@@ -5,13 +5,23 @@ const db = require('./data/db.js');
 
 const server = express();
 
-server.get('/', (req, res) => {
+server.use(express.json());
+
+server.get('/api', (req, res) => {
     res.send('BackEnd Week 1 Day 1 Project');
 })
 
+server.get('/api/users', (req, res) => {
+    db.find()
+        .then( users => {
+            res.status(200).json(users);
+        })
+        .catch(err => {
+            res.status(500).json({ error: "The users information could not be retrieved." })
+        });
+});
 
-
-
+//At the bottom
 server.listen(4000, () => {
     console.log('\n** API up and running on port 4k **');
 });
