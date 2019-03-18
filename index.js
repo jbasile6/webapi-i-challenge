6d5e45c756a9757698d44d392e22e3806c51cd7a
James Basile <jbasile145@gmail.com>
2019-03-18 15:28:25 -0400

    init commit, installed express dependencies

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // implement your API here
+const express = require('express');
+
+const db = require('./data/db.js');
+
+const server = express();
+
+server.get('/', (req, res) => {
+    res.send('BackEnd Week 1 Day 1 Project');
+})
+
+
+
+
+server.listen(4000, () => {
+    console.log('\n** API up and running on port 4k **');
+});
